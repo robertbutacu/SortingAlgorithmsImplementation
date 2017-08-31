bfd43e7dc399aec4d8093ca4bddfa364efcf4bcf
robert.butacu@yahoo.com <robert.butacu@yahoo.com>
2017-08-31 16:54:30 +0300

    Added explanations of the algorithms.

--- src/main/scala/NLogN/MergeSort.scala
+++ src/main/scala/NLogN/MergeSort.scala
@@ -5,6 +5,19 @@ import scala.annotation.tailrec
 /**
   * Created by Robert-PC on 8/29/2017.
   */
+/**
+  * There are 2 important points when memorising mergesort:
+  *   1. merge() receiving 2 ordered lists and "merging" them
+  *     Ex: 5 8 32 40         4 9 35 50
+  *             4 5 8 9 32 35 40 50
+  *   Of course, you almost never get 2 sorted arrays if you were to split a big one.
+  *   2. recursively split the list in half until there is only 1 element left, and then start sorting.
+  *     Ex: 9 4 8 7 1 5 => 6 elements, first two elements will be sorted first => 4 9, then 7 8, then 1 5.
+  *       Following that, 4 9 and 7 8 will be sorted => 4 7 8 9, and in the end, with 1 5 => 1 4 5 6 7 8.
+  *
+  *   Very good video : https://www.youtube.com/watch?v=EeQ8pwjQxTM  .
+  */
+
 trait MergeSort {
   def mergesort(xs: List[Int]): List[Int] = {
     @tailrec
--- src/main/scala/NLogN/QuickSort.scala
+++ src/main/scala/NLogN/QuickSort.scala
@@ -3,6 +3,17 @@ package NLogN
 /**
   * Created by Robert-PC on 8/29/2017.
   */
+
+/**
+  *   The beauty of PF is shown in the Quicksort algorithm.
+  *   Instead of memorising anything about the "wall", etc about quicksort, this is how quicksort works:
+  *     1. pick a pivot
+  *     2. create a list containing a recursive call to quicksort(elements smaller than pivot),
+  *       concatenated with current element, with quicksort(elements bigger than pivot).
+  *   That's it : 4 lines, compared to around 20 of imperative programming.
+  *
+  *   Slightly complicated video about this: https://www.youtube.com/watch?v=aQiWF4E8flQ    .
+  */
 trait QuickSort {
   def quicksort(list: List[Int]): List[Int] = {
     list match {
